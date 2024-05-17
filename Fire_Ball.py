fcd8cc3d31aaa6b5fe51645a58fc84e76e228094
Birds Are real <146360282+Wisparity@users.noreply.github.com>
2024-05-17 09:11:57 -0600

    Update Fire_Ball.py

--- Fire_Ball.py
+++ Fire_Ball.py
@@ -1,4 +1,5 @@
 import pygame
+import math 
 #CONSTANTS
 LEFT = 0 
 RIGHT = 1 
@@ -17,6 +18,23 @@ class fireball:
         self.xpos = x + 20 
         self.ypos = y + 20 
         self.isAlive = True 
-        self.direction = dir 
-
-
+        self.direction = dir
+    def move(self):
+        if self.direction == RIGHT: 
+            self.xpos+=20 
+        elif self.direction == LEFT:
+            self.xpos-=20
+        if self.direction == DOWN: 
+            self.ypos+=20 
+        elif self.direction == UP:
+            self.ypos-=20
+    def draw(self,screen):
+        pygame.draw.circle(screen, (250,0,0), (self.xpos,self.ypos),10)
+        pygame.draw.circle(screen, (250,250,0), (self.xpos,self.ypos),5)
+    def collide (self,x,y):
+        #distance formula
+        if math.sqrt((self.xpos - x) ** 2 + (self.ypos - y) ** 2) <25:
+            print("collision!")
+            return True 
+        else: 
+            return False
